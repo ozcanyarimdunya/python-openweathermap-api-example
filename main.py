5dc916dfda3a1bbdf5e557ad5d170203fb9c3083
ozcaan11 <ozcanyd@gmail.com>
2016-12-23 12:21:43 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+"""
+    :key => 814c59b24b1da8eca1db0b0e28a04c1c
+    :url => http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=:key
+"""
+
+from urllib.request import urlopen
+import json
+
+_id = '315202'  # id for Eskisehir
+key = '814c59b24b1da8eca1db0b0e28a04c1c'   # api key
+url = 'http://api.openweathermap.org/data/2.5/forecast/city?id=' + _id + '&APPID=' + key   # full url
+
+response = urlopen(url).read().decode('utf-8')
+obj = json.loads(response)
+name = obj['city']['name']
+details = []
+for state in obj['list']:
+    detail = """
+    Time : {}
+        MAIN
+            Temperature : {}
+        WIND
+            Degree : {}
+            Speed  : {}
+        WEATHER
+            Description : {}
+            Main        : {}
+
+""".format(state['dt_txt'][11:16],
+           state['main']['temp_kf'],
+           state['wind']['deg'],
+           state['wind']['speed'],
+           state['weather'][0]['description'],
+           state['weather'][0]['main'] + "\n" + "-" * 50)
+    print(detail)
+    details.append(detail)
+
+with open('result', 'w') as fp:
+    fp.write(str(name).upper() + " WEATHER\n" + "-" * 50 + "\n")
+    for detail in details:
+        fp.write(detail)
